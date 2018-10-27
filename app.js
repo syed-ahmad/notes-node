ab79790c54e12071daf3fdf73a67cd89b3c55bd2
Syed Ahmad <syed-ahmad@outlook.com>
2018-10-28 00:12:01 +0100

    Use require to install third party module lodash

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ console.log('Starting app...');
 
 const fs = require('fs');
 const os = require('os');
+// 1. Look for core module lodash
+// 2. Look inside node_modules folder
+const _ = require('lodash');
 const notes = require('./notes');
 
-var res = notes.add;
-console.log('Result: ', res(2,3));
+var filteredArray = _.uniq(['syed', 1, 'syed', 2, 3, 4, 1]);
+console.log(filteredArray);
+
+// console.log(_.isString(true));
+// console.log(_.isString('Syed'));
+
+// var res = notes.add;
+// console.log('Result: ', res(2,3));
 
 // var user = os.userInfo();
 
